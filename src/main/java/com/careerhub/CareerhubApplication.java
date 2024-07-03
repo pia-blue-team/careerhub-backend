11df7ac247a8e1f1a7cd3228a57c9fe2c1023cc6
Berkay Dinç <berkayydinc@gmail.com>
2024-07-03 17:19:59 +0300

    Create Maven project boilerplate

---
+++ src/main/java/com/careerhub/CareerhubApplication.java
@@ -0,0 +1,13 @@
+package com.careerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerhubApplication.class, args);
+	}
+
+}
