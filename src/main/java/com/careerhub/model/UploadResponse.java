5bce5a24b2b61699bb3e4ac24c58cc255eea9979
Emirhan Şengül <emirhansengul41@hotmail.com>
2024-07-07 18:18:18 +0300

    FileUpload

---
+++ src/main/java/com/careerhub/model/UploadResponse.java
@@ -0,0 +1,23 @@
+package com.careerhub.model;
+
+public class UploadResponse {
+    private final String fileName;
+    private final String fullName;
+    private final String dateOfBirth;
+
+    public UploadResponse(String fileName, String fullName, String dateOfBirth) {
+        this.fileName = fileName;
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+}
