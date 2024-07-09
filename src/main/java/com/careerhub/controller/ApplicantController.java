6b83d2de2f936b3852d38efc80549ebc80de7b8a
Melike Nazli Karaca <melikenazlikaraca@gmail.com>
2024-07-09 15:19:57 +0300

    Bug fix

--- src/main/java/com/careerhub/controller/ApplicantController.java
+++ src/main/java/com/careerhub/controller/ApplicantController.java
@@ -9,11 +9,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -22,7 +20,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 
-@Service
+@Controller
+@RequestMapping("/careerhub")
 public class ApplicantController {
 
     @Autowired
